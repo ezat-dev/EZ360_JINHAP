9db50c12e9256e5c912248c9b9108fe4b48000e2
EZ <hijko@ezat.co.kr>
2025-07-30 15:49:17 +0900

    0730

--- src/main/java/com/geomet/controller/ConditionController.java
+++ src/main/java/com/geomet/controller/ConditionController.java
@@ -29,6 +29,7 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -37,6 +38,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -868,5 +870,6 @@ public class ConditionController {
          return u;
       }
 
-    
+   
+
 }
